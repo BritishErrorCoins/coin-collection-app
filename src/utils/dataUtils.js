f89f4387ad82e571f0930cba949063e63dad2ab6
BritishErrorCoins <jrenaud001@gmail.com>
2025-06-01 22:11:53 +0100

    Export addToCollection and addToWantlist from dataUtils

--- src/utils/dataUtils.js
+++ src/utils/dataUtils.js
@@ -1,3 +1,14 @@
 export function getUniqueValues(data, key) {
   return [...new Set(data.map((item) => item[key]).filter(Boolean))].sort();
 }
+export function addToCollection(newCoin) {
+  const current = JSON.parse(localStorage.getItem("collection") || "[]");
+  current.push(newCoin);
+  localStorage.setItem("collection", JSON.stringify(current));
+}
+
+export function addToWantlist(newItem) {
+  const current = JSON.parse(localStorage.getItem("wantlist") || "[]");
+  current.push(newItem);
+  localStorage.setItem("wantlist", JSON.stringify(current));
+}
